d317bb89b7667d173a2e6b195ed7ffb56c3a623e
SkyRider030 <et21063@lehre.dhbw-stuttgart.de>
2024-02-16 10:56:48 +0100

    Funkt

--- main.py
+++ main.py
@@ -31,8 +31,6 @@ def predict_image(image):
 def detect_objects(image_path):
     # Laden des Bildes
     image = cv2.imread(image_path)
-    cv2.imshow("Bild", image)
-    cv2.waitKey(0)
     
     # Umwandlung von BGR in RGB
     image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
@@ -44,7 +42,7 @@ def detect_objects(image_path):
 
 if __name__ == "__main__":
     # Beispielaufruf der Funktion zur Objekterkennung
-    image_path = 'C:\\Users\\Hendrik\\Downloads\\Praktiukm_Java\\example.jpg'  # Passe den Dateipfad zu deinem Bild an
+    image_path = 'example.jpg'  # Passe den Dateipfad zu deinem Bild an
 
     prediction = detect_objects(image_path)
     print("Prediction:", prediction)
